da3ae64025d26b4583d6fc3c49d92f8745ddccc9
unknown <kwhkim>
2022-09-11 19:36:03 +0900

    ver 0.1

---
+++ R/han2eng2han.R
@@ -0,0 +1,83 @@
+createFunc_case_switch = function() {
+  id <- paste0(
+    paste0("'",letters,"' <> '", LETTERS, "';\n"),
+    paste0("'",LETTERS,"' <> '", letters, "';\n"), collapse='')
+  func = function(x) {
+    stringi::stri_trans_general(x, id=id, rules=TRUE)
+  }
+}
+
+createFunc_eng_to_han = function() {
+  func = function(x) {
+    KoNLP::HangulAutomata(x,
+                          isForceConv=TRUE,
+                          isKeystroke=TRUE)
+  }
+}
+
+createFunc_han_to_eng = function() {
+  func = function(x) {
+    paste0(
+      KoNLP::convertHangulStringToKeyStrokes(x, isFullwidth=FALSE),
+      collapse = '')
+  }
+}
+
+createFunc_to_strokes = function() {
+  id <- paste0(
+    paste0("'",LETTERS[1:4],"' > '", letters[1:4], "';\n", collapse = ''),
+    paste0("'",LETTERS[6:14],"' > '", letters[6:14], "';\n", collapse = ''),
+    paste0("'",LETTERS[19:26][c(-2,-5)],"' > '", letters[19:26][c(-2,-5)], "';\n", collapse = ''),
+    collapse='')
+  func = function(x) {
+    stringi::stri_trans_general(x, id=id, rules=TRUE)
+  }
+}
+
+eng_to_han = createFunc_eng_to_han()
+han_to_eng = createFunc_han_to_eng()
+to_strokes = createFunc_to_strokes()
+case_switch = createFunc_case_switch()
+
+change_keystrokes <- function() {
+  # Get the active document
+  ctx <- rstudioapi::getActiveDocumentContext()
+
+  if (!is.null(ctx)) {
+    selected_text <- ctx$selection[[1]]$text
+    cat("* selected :", selected_text, "\n")
+    n_han = stringi::stri_count_regex(selected_text, "\\p{Hangul}")
+    n_eng = stringi::stri_count_regex(selected_text, "[A-Za-z]")
+    cat("  - n_han:", n_han, "\n")
+    cat("  - n_eng:", n_eng, "\n")
+    if (n_han > n_eng) {
+      selected_text = han_to_eng(selected_text)
+    } else {
+      selected_text2 = eng_to_han(selected_text)
+      n_eng = stringi::stri_count_regex(selected_text2, "[A-Za-z]")
+      if (n_eng > 0) {
+        selected_text =eng_to_han(to_strokes(selected_text))
+      } else {
+        selected_text = selected_text2
+      }
+
+    }
+
+    rstudioapi::modifyRange(ctx$selection[[1]]$range, selected_text)
+    #print(str(x))
+    #print(x)
+    #rstudioapi::setSelectionRanges(x$ranges)
+  }
+}
+
+change_case = function() {
+
+  ctx <- rstudioapi::getActiveDocumentContext()
+
+  if (!is.null(ctx)) {
+    selected_text <- ctx$selection[[1]]$text
+    #cat("* selected :", selected_text, "\n")
+    selected_text <- case_switch(selected_text)
+    rstudioapi::modifyRange(ctx$selection[[1]]$range, selected_text)
+  }
+}
